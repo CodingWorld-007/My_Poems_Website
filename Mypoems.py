ce10e852b4a8d013b4c14d3eecc35e2bcadc3481
Aman Joshi <139854820+CodingWorld-007@users.noreply.github.com>
2024-06-23 23:12:17 +0530

    Update Mypoems.py

--- Mypoems.py
+++ Mypoems.py
@@ -1,20 +1,27 @@
 import streamlit as st
 from PIL import Image
+import base64
+import io
+from pathlib import Path
 
 # Function to load images
 def load_image(image_path):
     return Image.open(image_path)
+
+# Function to convert image to base64
+def image_to_base64(image):
+    buffered = io.BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    return img_str
 
 # Function to load and inject CSS
 def load_css(css_file_path):
     with open(css_file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-# Load external CSS file
+# Load external CSS file (assuming you have a 'styles.css' file in the same directory)
 load_css("styles.css")
-
-# Add a sidebar with poem options
-st.sidebar.header("Choose a Poem")
 
 # Define poem options
 poem_options = {
@@ -26,7 +33,7 @@ poem_options = {
     "Reality is Money": "Poems/Poem6.jpg"
 }
 
-# Create a select box in the sidebar for poem selection
+# Main content
 selected_poem = st.sidebar.selectbox("Click below:", list(poem_options.keys()))
 
 # Set the title of the app dynamically based on the selected poem
@@ -35,9 +42,18 @@ st.title(f"POEM - {selected_poem}")
 # Load and display the selected poem image
 if selected_poem:
     image_path = poem_options[selected_poem]
-    st.sidebar.markdown("---")
     image = load_image(image_path)
+    st.markdown("---")
+    # Download button to download poem image
+    if st.button("Download Poem"):
+        img_base64 = image_to_base64(image)
+        href = f'data:image/jpeg;base64,{img_base64}'
+        st.markdown(f'<a href="{href}" download="{selected_poem}.jpg">Click here to download the Image</a>', unsafe_allow_html=True)
+    st.markdown("---")
     st.image(image, caption=selected_poem, use_column_width=True)
+    st.markdown("---")
+    st.sidebar.markdown("---")
     if st.sidebar.button("About Author"):
+        st.sidebar.markdown("---")
         st.sidebar.markdown("[LinkedIn](www.linkedin.com/in/amanajjoshi)")
-# Add button for "About Author"
+    
